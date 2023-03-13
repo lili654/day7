95b5a9673e40399474e7e8ad2c8c7493da3e7611
lily <103358311+lili654@users.noreply.github.com>
2023-03-13 16:19:30 +0800

    Update ll.cpp

--- ll.cpp
+++ ll.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    int n[100];
-    n[1]=1;
-    for(int i=2;i<=20;i++)
-    {
-        n[i]=n[i-1]+4*(i-1);
+  // 请在此输入您的代码
+  int months[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+  int year,month,day;
+  int week=6;
+  int km=0;
+  for(year=2000;year<=2020;year++){
+    if(year%4==0&&year%100!=0||year%400==0)
+    months[2]=29;
+    else 
+    months[2]=28;
+    for(month=1;month<=12;month++){
+      for(day=1;day<=months[month];day++){
+        km++;
+        if(week==8)
+        week=1;
+        if(week==1||day==1)
+        km++;
+        week++;
+        if(year==2020&&month==10&&day==1)
+        cout<<km;
+      }
     }
-    cout<<n[20];
-    return 0;
+  }
+  return 0;
 }
